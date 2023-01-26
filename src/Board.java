9a39915e1eff3b697d1ae64bdb2e78e3dab2379f
RealDwoot <dennis_losett23@milton.edu>
2023-01-26 13:50:21 -0500

    -Chomp 3x3 MyPlayer works!!

---
+++ src/Board.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Board {
+    ArrayList<Integer> boardState = new ArrayList<Integer>();
+    int moveToMakeRow;
+    int moveToMakeCol;
+   // boolean isLosing;
+}
